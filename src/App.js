0cfef01f75925113ff09c78638e1c89ae8046a03
Neha <neha.mishra@publicissapient.com>
2020-11-13 01:42:04 +0530

    change the image to image3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://wishlist-image.s3.us-east-2.amazonaws.com/image2.jpg" alt="wishlist" width="400px" height="400px"/>
+        <img src="https://wishlist-image.s3.us-east-2.amazonaws.com/image3.jpg" alt="wishlist" width="400px" height="400px"/>
         <p>
           Project 1 Demo React App
         </p>
